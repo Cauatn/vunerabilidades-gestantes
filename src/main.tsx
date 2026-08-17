d5e1a1a859b72de83648b3ee88e8091b9405fa46
cauatn <tavarescauac@gmail.com>
2026-08-17 20:29:39 -0300

    Inicializa projeto com Vite, Tailwind, TanStack Query, nuqs e axios

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { NuqsAdapter } from 'nuqs/adapters/react'
+import './index.css'
+import App from './App.tsx'
+
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <NuqsAdapter>
+        <App />
+      </NuqsAdapter>
+    </QueryClientProvider>
+  </StrictMode>,
+)
